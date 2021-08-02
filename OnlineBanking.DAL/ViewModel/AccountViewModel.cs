e07d3d9d9026b92e6d2f8a6c5ff16706fe84453d
thuyetbn <thuyetbn96@gmail.com>
2021-08-02 21:14:18 +0700

    Update Account Admin

---
+++ OnlineBanking.DAL/ViewModel/AccountViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBanking.DAL
+{
+    public class AccountViewModel
+    {
+        public int AccountId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Birthday { get; set; }
+        public int? Status { get; set; } // active, delete, lock
+        public int? RoleId { get; set; } // Quyền
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
+        public string Action { get; set; }
+
+
+
+    }
+}
