571df9d3de3b9d83dc2683b08eb01b8288032ebb
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2021-08-02 23:29:34 +0700

    Update seeder currency

--- OnlineBanking.DAL/Migrations/Configuration.cs
+++ OnlineBanking.DAL/Migrations/Configuration.cs
@@ -36,13 +36,20 @@ namespace OnlineBanking.DAL.Migrations
                     Status = 1
                 }
                 );
+
             context.Accounts.AddOrUpdate(x => x.AccountId,
                 new Accounts() { AccountId = 1, Name = "Admin", Email = "admin@gmail.com", Password = "123456", RoleId = 1, Status = 1 },
                 new Accounts() { AccountId = 2, Name = "Telesupport 1", Email = "tele@gmail.com", Password = "123456", RoleId = 2, Status = 1 },
                 new Accounts() { AccountId = 3, Name = "Telesupport 2", Email = "tele2@gmail.com", Password = "123456", RoleId = 2, Status = 1 },
                 new Accounts() { AccountId = 4, Name = "User 1", Email = "user@gmail.com", Password = "123456", RoleId = 3, Status = 1 },
                 new Accounts() { AccountId = 5, Name = "User 2", Email = "user2@gmail.com", Password = "123456", RoleId = 3, Status = 1 }
-        );
+            );
+
+            context.Currencies.AddOrUpdate(x => x.CurrencyId,
+                new Currencies() { CurrencyId = 1, Name = "VND" },
+                new Currencies() { CurrencyId = 2, Name = "USD" },
+                new Currencies() { CurrencyId = 3, Name = "EUR" }
+            );
 
 
         }
