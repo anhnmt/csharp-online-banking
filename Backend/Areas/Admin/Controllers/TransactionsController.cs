1d7d8229d1d3be58ad169203bb9677ad83a4649c
thuyetbn <thuyetbn96@gmail.com>
2021-08-04 21:42:10 +0700

    update

---
+++ Backend/Areas/Admin/Controllers/TransactionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Backend.Areas.Admin.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        // GET: Admin/Transactions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
