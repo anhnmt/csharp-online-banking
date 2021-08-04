d5e139dad88d93f51fc97c2dea1e0c65bfebb21a
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2021-08-05 00:57:15 +0700

    Update chat form

---
+++ OnlineBanking.DAL/DataMapping/Channels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBanking.DAL
+{
+    public class Channels : BaseModel
+    {
+        public Channels()
+        {
+            Messages = new HashSet<Messages>();
+        }
+
+        [Key]
+        public int ChannelId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+
+        public ICollection<Messages> Messages { get; set; }
+    }
+}
---
+++ OnlineBanking.DAL/DataMapping/Messages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBanking.DAL
+{
+    public class Messages
+    {
+        [Key]
+        public int MessageId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public int ChannelId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public int AccountId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Content { get; set; }
+
+        public DateTime? Timestamp { get; set; }
+
+        //[ForeignKey("AccountId")]
+        //public virtual Accounts Account { get; set; }
+
+        //[ForeignKey("ChannelId")]
+        //public virtual Channels Channel { get; set; }
+    }
+}
