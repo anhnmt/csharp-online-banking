fb39cd41f8dc7f22cee1bcef9e691bd379546eee
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2021-08-04 18:06:32 +0700

    Update validate

---
+++ Backend/Areas/Admin/Content/js/custom-validate.js
@@ -0,0 +1,17 @@
+﻿jQuery.validator.setDefaults({
+    highlight: function (element) {
+        jQuery(element).closest('.form-control').addClass('is-invalid');
+    },
+    unhighlight: function (element) {
+        jQuery(element).closest('.form-control').removeClass('is-invalid');
+    },
+    errorElement: 'span',
+    errorClass: 'invalid-feedback',
+    errorPlacement: function (error, element) {
+        if (element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
