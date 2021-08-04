f038ca7b01587c9e243a4f39423fe1527ba131cf
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2021-08-04 17:44:44 +0700

    Update validate

---
+++ Backend/Areas/Admin/Data/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Areas.Admin.Data
+{
+    public static class Utils
+    {
+        public static bool IsAny<T>(this IEnumerable<T> data)
+        {
+            return data != null && data.Any();
+        }
+    }
+}
