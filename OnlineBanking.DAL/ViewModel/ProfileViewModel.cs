a176d4e4ff8b5e0f7cc6a5a134a91fa0d1154d97
doremon04 <minhnt141204@gmail.com>
2021-08-05 18:56:13 +0700

    update profile

---
+++ OnlineBanking.DAL/ViewModel/ProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBanking.DAL
+{
+    public class ProfileViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Birthday { get; set; }
+        public string Address { get; set; }
+        public int? NumberID { get; set; }
+        public string StatusName { get; set; } // active, delete, lock
+        public string RoleName { get; set; } // Quyền
+    }
+}
