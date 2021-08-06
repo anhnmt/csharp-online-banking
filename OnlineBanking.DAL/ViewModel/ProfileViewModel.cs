41ceabcd678d7534aa54ce331323e4191e1dd592
doremon04 <minhnt141204@gmail.com>
2021-08-06 17:03:31 +0700

    Update profile validation

--- OnlineBanking.DAL/ViewModel/ProfileViewModel.cs
+++ OnlineBanking.DAL/ViewModel/ProfileViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,15 @@ namespace OnlineBanking.DAL
 {
     public class ProfileViewModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Birthday { get; set; }
         public string Address { get; set; }
-        public int? NumberID { get; set; }
+        public string NumberID { get; set; }
         public string StatusName { get; set; } // active, delete, lock
         public string RoleName { get; set; } // Quyền
     }
