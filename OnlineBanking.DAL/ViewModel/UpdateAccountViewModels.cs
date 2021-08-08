fd50e2a77eb46f15f01e887360391a673a07d0a5
doremon04 <minhnt141204@gmail.com>
2021-08-08 18:39:47 +0700

    update select 2 on create cheque books

---
+++ OnlineBanking.DAL/ViewModel/UpdateAccountViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBanking.DAL
+{
+    public class UpdateAccountViewModels
+    {
+        public int AccountId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Birthday { get; set; }
+        public string Address { get; set; }
+        public string NumberID { get; set; }
+        public int? Status { get; set; } // active, delete, lock
+        public string UpdatedAt { get; set; }
+    }
+}
