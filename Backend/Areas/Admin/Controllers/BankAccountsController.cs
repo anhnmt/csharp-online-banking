1e8ea75e8a578da9f259e89f0482add321a02587
thuyetbn <thuyetbn96@gmail.com>
2021-08-12 21:20:10 +0700

    Update validation Account, Transfers validation

--- Backend/Areas/Admin/Controllers/BankAccountsController.cs
+++ Backend/Areas/Admin/Controllers/BankAccountsController.cs
@@ -1,7 +1,10 @@
 ﻿using OnlineBanking.BLL.Repositories;
 using OnlineBanking.DAL;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 namespace Backend.Areas.Admin.Controllers
@@ -28,6 +31,7 @@ namespace Backend.Areas.Admin.Controllers
                 .FirstOrDefault();
             return data == null ? View() : View(data);
         }
+
         [HttpPost]
         public ActionResult FindId(int id)
         {
@@ -35,6 +39,7 @@ namespace Backend.Areas.Admin.Controllers
             var data = new BankAccountsViewModels(x);
             return Json(data, JsonRequestBehavior.AllowGet);
         }
+
         [HttpPost]
         public ActionResult GetBalance(int bankId)
         {
@@ -98,46 +103,96 @@ namespace Backend.Areas.Admin.Controllers
         [HttpPost]
         public ActionResult Create(BankAccounts bank)
         {
+            var errors = new Dictionary<string, string>();
+            var check = true;
+
+            if (!int.TryParse(bank.Name, out int i))
+            {
+                check = false;
+                errors.Add("NameBank", "Your name must be number");
+            }
             
-            if (!ModelState.IsValid)
+            if (bankAccounts.CheckDuplicate(x => x.Name == bank.Name))
+            {
+                check = false;
+                errors.Add("NameBank", "Your Name has been used!");
+            }
+
+            if (check && ModelState.IsValid)
+            {
+                bankAccounts.Add(bank);
                 return Json(new
                 {
-                    statusCode = 402,
-                    message = "Error",
-                    data = bank
+                    statusCode = 200,
+                    message = "Success"
                 }, JsonRequestBehavior.AllowGet);
-            bankAccounts.Add(bank);
+            }
+
+            foreach (var k in ModelState.Keys)
+            foreach (var err in ModelState[k].Errors)
+            {
+                var key = Regex.Replace(k, @"(\w+)\.(\w+)", @"$2");
+                if (!errors.ContainsKey(key))
+                    errors.Add(key, err.ErrorMessage);
+            }
+
             return Json(new
             {
-                statusCode = 200,
-                message = "Success"
+                statusCode = 400,
+                message = "Error",
+                data = errors
             }, JsonRequestBehavior.AllowGet);
         }
 
-        // POST: Admin/BankAccounts/Edit/5
         [HttpPost]
         public ActionResult Edit(BankAccounts bank)
         {
-            if (!ModelState.IsValid)
+            var errors = new Dictionary<string, string>();
+            var check = true;
+            var bank1 = bankAccounts.Get(bank.BankAccountId);
+            
+            if (!int.TryParse(bank.Name, out int i))
+            {
+                check = false;
+                errors.Add("NameBank", "Your name must be number");
+            }
+
+            if (bankAccounts.CheckDuplicate(x => x.Name == bank.Name && x.BankAccountId != bank.BankAccountId))
+            {
+                check = false;
+                errors.Add("NameBank", "Your Name has been used!");
+            }
+
+            if (check && ModelState.IsValid)
+            {
+                bank1.CurrencyId = bank.CurrencyId;
+                bank1.Name = bank.Name;
+                bank1.Balance = bank.Balance;
+                bank1.Status = bank.Status;
+                bankAccounts.Edit(bank1);
                 return Json(new
                 {
-                    statusCode = 402,
-                    message = "Error",
-                    data = bank
+                    statusCode = 200,
+                    message = "Success"
                 }, JsonRequestBehavior.AllowGet);
-            var bank1 = bankAccounts.Get(bank.BankAccountId);
-            bank1.AccountId = bank.AccountId;
-            bank1.CurrencyId = bank.CurrencyId;
-            bank1.Name = bank.Name;
-            bank1.Balance = bank.Balance;
-            bank1.Status = bank.Status;
-            bankAccounts.Edit(bank1);
+            }
+
+            foreach (var k in ModelState.Keys)
+            foreach (var err in ModelState[k].Errors)
+            {
+                var key = Regex.Replace(k, @"(\w+)\.(\w+)", @"$2");
+                if (!errors.ContainsKey(key))
+                    errors.Add(key, err.ErrorMessage);
+            }
+
             return Json(new
             {
-                statusCode = 200,
-                message = "Success"
+                statusCode = 400,
+                message = "Error",
+                data = errors
             }, JsonRequestBehavior.AllowGet);
         }
+
         [HttpPost]
         public ActionResult Delete(int id)
         {
